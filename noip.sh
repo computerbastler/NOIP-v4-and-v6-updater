a24ea2ae9740f37a1b13398f9da8f600862ea866
computerbastler <42007612+computerbastler@users.noreply.github.com>
2024-03-24 13:25:23 +0100

    Update noip.sh
    optimazion

--- noip.sh
+++ noip.sh
@@ -1,31 +1,10 @@
 #!/bin/bash
-USERNAME="input username"
-PASSWORD="input password"
-HOST="input host adress name"
+USERNAME="Benutzer"
+PASSWORD="Password"
+HOST="Name.ddns.net"
 USERAGENT="Datenreise NOIP Updater/0.4"
-INTERFACE="input Networkinterface"
-
-# Get initial IP address
-IP4=$(curl -s http://checkip.dyndns.org | sed -e 's/.*Current IP Address: //' -e 's/<.*$//')
+INTERFACE="enp1s0"
+IP4=$(curl -s https://api.ipify.org)
 IP6=$(ip -6 addr show dev $INTERFACE | awk '/inet6/ {print $2}' | cut -d/ -f1 | head -n 1)
-
-while true; do
-  # Get current IP address
-  CURRENT_IP4=$(curl -s http://checkip.dyndns.org | sed -e 's/.*Current IP Address: //' -e 's/<.*$//')
-  CURRENT_IP6=$(ip -6 addr show dev enp2s0 | awk '/inet6/ {print $2}' | cut -d/ -f1 | head -n 1)
-
-  # Compare current IP to previous IP
-  if [ "$CURRENT_IP4" != "$IP4" ]; then
-    # Update IP address
-    IP4=$CURRENT_IP4
-    curl -s -k -u $USERNAME:$PASSWORD --user-agent "$USERAGENT" "https://dynupdate.no-ip.com/nic/update?hostname=$HOST&myip=$IP4"
-  fi
-  if [ "$CURRENT_IP6" != "$IP6" ]; then
-    # Update IP address
-    IP6=$CURRENT_IP6
-    curl -s -k -u $USERNAME:$PASSWORD --user-agent "$USERAGENT" "https://dynupdate.no-ip.com/nic/update?hostname=$HOST&myip=$IP6"
-  fi
-
-  # Sleep for 30 seconds before next IP check
-  sleep 30
-done
+curl -s -k -u $USERNAME:$PASSWORD --user-agent "$USERAGENT" "https://dynupdate.no-ip.com/nic/update?hostname=$HOST&myip=$IP4"
+curl -s -k -u $USERNAME:$PASSWORD --user-agent "$USERAGENT" "https://dynupdate.no-ip.com/nic/update?hostname=$HOST&myip=$IP6"
